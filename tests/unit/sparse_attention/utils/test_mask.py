82eace780186b907ff05fe0175517e8f17fdb1e8
Aditya Desai <apd10@rice.edu>
2025-07-03 17:50:06 -0700

    Add Tests for apply_mask and create_empty_mask

--- tests/unit/sparse_attention/utils/test_mask.py
+++ tests/unit/sparse_attention/utils/test_mask.py
@@ -4,64 +4,101 @@ import numpy as np
 import pytest
 
 @pytest.mark.unit
-def test_create_mask_from_dense_mask():
-    shape = (3, 5)
-    mask = torch.tensor([[1.0, 0.0, 1.0, 0.0, 1.0], [0.0, 1.0, 0.0, 1.0, 0.0], [1.0, 0.0, 1.0, 0.0, 1.0]])
-    mask_object = Mask.create_mask_from_dense_mask(shape, mask)
-    assert mask_object.shape == shape
-    assert torch.allclose(mask_object.mask, mask)
-    assert mask_object.from_dense_mask
+class TestMask:
+    def test_create_mask_from_dense_mask(self):
+        shape = (3, 5)
+        mask = torch.tensor([[1.0, 0.0, 1.0, 0.0, 1.0], [0.0, 1.0, 0.0, 1.0, 0.0], [1.0, 0.0, 1.0, 0.0, 1.0]])
+        mask_object = Mask.create_mask_from_dense_mask(shape, mask)
+        assert mask_object.shape == shape
+        assert torch.allclose(mask_object.mask, mask)
+        assert mask_object.from_dense_mask
 
-def test_get_index_mask():
-    shape = (3, 5)
-    ptr = torch.tensor([0, 5, 7, 9])
-    indices = torch.tensor([0, 1, 2, 3, 4, 0, 1, 2, 3])
-    data = torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
-    index_mask_object = Mask.create_mask_from_indices(shape, indices, ptr, data)
-    index_mask, index_ptr, index_data = index_mask_object.get_index_mask()
-    assert torch.allclose(index_mask, indices)
-    assert torch.allclose(index_ptr, ptr)
-    assert torch.allclose(index_data, data)
-    assert index_mask_object.from_index
+    def test_get_index_mask(self):
+        shape = (3, 5)
+        ptr = torch.tensor([0, 5, 7, 9])
+        indices = torch.tensor([0, 1, 2, 3, 4, 0, 1, 2, 3])
+        data = torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+        index_mask_object = Mask.create_mask_from_indices(shape, indices, ptr, data)
+        index_mask, index_ptr, index_data = index_mask_object.get_index_mask()
+        assert torch.allclose(index_mask, indices)
+        assert torch.allclose(index_ptr, ptr)
+        assert torch.allclose(index_data, data)
+        assert index_mask_object.from_index
 
-def test_getters_dense_index():
-    shape = (3, 5)
-    mask = torch.tensor([[1.0, 0.0, 1.0, 0.0, 1.0], [0.0, 1.0, 0.0, 1.0, 0.0], [1.0, 0.0, 1.0, 0.0, 1.0]])
-    dense_mask_object = Mask.create_mask_from_dense_mask(shape, mask)
-    indices, ptr, data = dense_mask_object.get_index_mask()
-    
-    index_mask_object = Mask.create_mask_from_indices(shape, indices, ptr, data)
-    dense_mask = index_mask_object.get_dense_mask()
-    assert torch.allclose(dense_mask, mask)
+    def test_getters_dense_index(self):
+        shape = (3, 5)
+        mask = torch.tensor([[1.0, 0.0, 1.0, 0.0, 1.0], [0.0, 1.0, 0.0, 1.0, 0.0], [1.0, 0.0, 1.0, 0.0, 1.0]])
+        dense_mask_object = Mask.create_mask_from_dense_mask(shape, mask)
+        indices, ptr, data = dense_mask_object.get_index_mask()
+        
+        index_mask_object = Mask.create_mask_from_indices(shape, indices, ptr, data)
+        dense_mask = index_mask_object.get_dense_mask()
+        assert torch.allclose(dense_mask, mask)
 
-def test_getters_index_dense():
-    shape = (3, 5)
-    ptr = torch.tensor([0, 5, 7, 9])
-    indices = torch.tensor([0, 1, 2, 3, 4, 5, 6, 12, 13])
-    data = torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
-    index_mask_object = Mask.create_mask_from_indices(shape, indices, ptr, data)
+    def test_getters_index_dense(self):
+        shape = (3, 5)
+        ptr = torch.tensor([0, 5, 7, 9])
+        indices = torch.tensor([0, 1, 2, 3, 4, 5, 6, 12, 13])
+        data = torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+        index_mask_object = Mask.create_mask_from_indices(shape, indices, ptr, data)
 
-    dense_mask = index_mask_object.get_dense_mask()
-    dense_mask_object = Mask.create_mask_from_dense_mask(shape, dense_mask)
-    _indices, _ptr, _data = dense_mask_object.get_index_mask()
-    assert _indices.shape == indices.shape
-    assert _ptr.shape == ptr.shape
-    assert _data.shape == data.shape
-    assert torch.allclose(indices, _indices)
-    assert torch.allclose(_ptr, ptr)
-    assert torch.allclose(_data, data)
+        dense_mask = index_mask_object.get_dense_mask()
+        dense_mask_object = Mask.create_mask_from_dense_mask(shape, dense_mask)
+        _indices, _ptr, _data = dense_mask_object.get_index_mask()
+        assert _indices.shape == indices.shape
+        assert _ptr.shape == ptr.shape
+        assert _data.shape == data.shape
+        assert torch.allclose(indices, _indices)
+        assert torch.allclose(_ptr, ptr)
+        assert torch.allclose(_data, data)
 
 
 
-def test_getters_dense_index_n_dims(n_dims=3):
-    shape = tuple([5] * n_dims)
-    mask = (torch.rand(shape) > 0.5) * torch.rand(shape)
-    dense_mask_object = Mask.create_mask_from_dense_mask(shape, mask)
-    indices, ptr, data = dense_mask_object.get_index_mask()
-    
-    for i in range(np.prod(shape[:-1])):
-        start = ptr[i]
-        end = ptr[i + 1]
-        data = data[start:end]
-        indices = indices[start:end]
-        assert torch.allclose(data, mask.view(-1)[indices])
+    def test_getters_dense_index_n_dims(self):
+        n_dims = 3
+        shape = tuple([5] * n_dims)
+        mask = (torch.rand(shape) > 0.5) * torch.rand(shape)
+        dense_mask_object = Mask.create_mask_from_dense_mask(shape, mask)
+        indices, ptr, data = dense_mask_object.get_index_mask()
+        
+        for i in range(np.prod(shape[:-1])):
+            start = ptr[i]
+            end = ptr[i + 1]
+            data = data[start:end]
+            indices = indices[start:end]
+            assert torch.allclose(data, mask.view(-1)[indices])
+
+    def test_create_empty_mask_dense(self):
+        shape = (3, 5, 7)
+        mask_object = Mask.create_empty_mask(shape, mask_type="dense")
+        assert mask_object.shape == shape
+        assert mask_object.mask.shape == shape
+        assert mask_object.mask.sum() == 0
+        assert mask_object.from_dense_mask
+        assert not mask_object.from_index
+
+    def test_create_empty_mask_index(self):
+        shape = (3, 5, 7)
+        mask_object = Mask.create_empty_mask(shape, mask_type="index")
+        mask = mask_object.get_dense_mask()
+        assert mask.shape == shape
+        assert mask.sum() == 0
+        assert not mask_object.from_dense_mask
+        assert mask_object.from_index
+
+    def test_apply_mask(self):
+        shape = (3, 5, 7)
+        input = torch.rand(shape)
+        dense_mask = (torch.rand(shape) > 0.5).float()
+
+        true_answer = input * dense_mask
+
+        dense_mask_object = Mask.create_mask_from_dense_mask(shape, dense_mask)
+        indices, ptr, data = dense_mask_object.get_index_mask()
+        index_mask_object = Mask.create_mask_from_indices(shape, indices, ptr, data)
+
+        dense_masked_data = dense_mask_object.apply_mask(input)
+        index_masked_data = index_mask_object.apply_mask(input)
+
+        assert torch.allclose(dense_masked_data, true_answer)
+        assert torch.allclose(index_masked_data, true_answer)
