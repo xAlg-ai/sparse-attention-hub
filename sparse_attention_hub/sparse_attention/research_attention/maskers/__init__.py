7f28740bf24d3537a4889e4c6f63b48d0cda0165
Aditya Desai <apd10@rice.edu>
2025-07-26 14:24:06 -0500

    Remove OpenEvolve from init for this commit

--- sparse_attention_hub/sparse_attention/research_attention/maskers/__init__.py
+++ sparse_attention_hub/sparse_attention/research_attention/maskers/__init__.py
@@ -1,11 +1,8 @@
 """Research attention maskers."""
 
 from .base import MaskerConfig, ResearchMasker
-from .openevolve import OpenEvolveMasker, OpenEvolveMaskerConfig
 
 __all__ = [
     "ResearchMasker",
     "MaskerConfig",
-    "OpenEvolveMasker",
-    "OpenEvolveMaskerConfig",
 ]
